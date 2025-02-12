01d9ba5d41160690e5a3a34ffcb5da43e510f860
yanrui <1417630746@qq.com>
2025-02-12 10:58:57 +0800

    “firstcommit

---
+++ astro.config.ts
@@ -0,0 +1,29 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
+
+export default defineConfig({
+  site: 'https://astro-theme-vitesse.netlify.app/',
+  server: {
+    port: 1977,
+  },
+  integrations: [
+    mdx(),
+    sitemap(),
+    UnoCSS({
+      injectReset: true,
+    }),
+    vue(),
+  ],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light-default',
+        dark: 'github-dark-default',
+      },
+      wrap: true,
+    },
+  },
+})
---
+++ src/site-config.ts
@@ -0,0 +1,102 @@
+export const siteConfig = {
+  author: '晏_Yries',
+  title: 'Yriesの杂货铺',
+  subtitle: '一个记录生活、技术、想法的小破栈',
+  description: '一个记录生活、技术、想法的小破栈',
+  image: {
+    src: '/hero.jpg',
+    alt: 'Website Main Image',
+  },
+  email: '1417630746@qq.com',
+  socialLinks: [
+    {
+      text: 'GitHub',
+      href: '',
+      icon: 'i-simple-icons-github',
+      header: 'i-ri-github-line',
+    },
+    {
+      text: 'Twitter',
+      href: '',
+      icon: 'i-simple-icons-x',
+      header: 'i-ri-twitter-x-line',
+    },
+    {
+      text: 'Linkedin',
+      href: '',
+      icon: 'i-simple-icons-linkedin',
+    },
+    {
+      text: 'Instagram',
+      href: '',
+      icon: 'i-simple-icons-instagram',
+    },
+    {
+      text: 'Youtube',
+      href: '',
+      icon: 'i-simple-icons-youtube',
+    },
+  ],
+  header: {
+    logo: {
+      src: '/favicon.svg',
+      alt: 'Logo Image',
+    },
+    navLinks: [
+      {
+        text: '博客',
+        href: '/blog',
+      },
+      {
+        text: '笔记',
+        href: '/blog/notes',
+      },
+      {
+        text: '关于',
+        href: '/blog/talks',
+      },
+      {
+        text: '项目',
+        href: '/projects',
+      },
+    ],
+  },
+  page: {
+    blogLinks: [
+      {
+        text: 'Blog',
+        href: '/blog',
+      },
+      {
+        text: 'Notes',
+        href: '/blog/notes',
+      },
+      {
+        text: 'Talks',
+        href: '/blog/talks',
+      },
+    ],
+  },
+  footer: {
+    navLinks: [
+      {
+        text: 'Posts Props',
+        href: '/posts-props',
+      },
+      {
+        text: 'Markdown Style',
+        href: '/md-style',
+      },
+      {
+        text: 'View on Astro',
+        href: 'https://astro.build/themes/details/vitesse-theme-for-astro/',
+      },
+      {
+        text: 'GitHub Repository',
+        href: 'https://github.com/kevinwong865/astro-theme-vitesse',
+      },
+    ],
+  },
+}
+
+export default siteConfig
